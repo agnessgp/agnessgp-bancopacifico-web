3f4808cedc79d3bc3e4884f3f388135c9a54cade
agnessgp <34318699+agnessgp@users.noreply.github.com>
2018-09-13 09:11:38 -0500

    Initial commit

---
+++ src/main/java/ec/agnessgp/bancopacifico/AgnessgpBancopacificoWebApplication.java
@@ -0,0 +1,12 @@
+package ec.agnessgp.bancopacifico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgnessgpBancopacificoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgnessgpBancopacificoWebApplication.class, args);
+	}
+}
---
+++ src/main/java/ec/agnessgp/bancopacifico/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ec.agnessgp.bancopacifico;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AgnessgpBancopacificoWebApplication.class);
+	}
+
+}
